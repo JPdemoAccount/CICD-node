da1b069cb0732cd55c076b6c004601bafc9ad9a2
JPdemoAccount <ctsao+admin@jpdemo.io>
2024-11-10 20:29:07 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const port = 8081;
 
+// Serve static files (HTML, CSS, JS, images) from the 'public' directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Route for the home page
 app.get('/', (req, res) => {
-  res.send('Hello, World!2024');
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
+// Start the server
 app.listen(port, () => {
   console.log(`App listening at http://localhost:${port}`);
 });
