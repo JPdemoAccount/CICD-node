5224beb7df4f558915f7107f2f1ba2de5e20bf04
Ubuntu <ubuntu@ip-10-0-14-174.ap-northeast-1.compute.internal>
2024-11-10 11:09:48 +0000

    Saving local changes before merge

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const app = express();
-const port = 80;
+const port = 8081;
 
 app.get('/', (req, res) => {
-  res.send('Hello, World!1');
+  res.send('Hello, World!11');
 });
 
-app.listen(port, () => {
+app.listen(port,'0.0.0.0', () => {
   console.log(`App listening at http://localhost:${port}`);
 });
